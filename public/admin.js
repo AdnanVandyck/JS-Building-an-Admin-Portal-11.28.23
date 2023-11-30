55f5edcc3bd67a97358898bcacd0dfbed11654c5
Adnan <adnanvandyck@gmail.com>
2023-11-30 01:06:50 -0500

    got admin page to display titles and quantities, as well as save

--- public/admin.js
+++ public/admin.js
@@ -1,11 +1,52 @@
-async function admin() {
-let response = await fetch('http://localhost:3001/listBooks')
-let books = await response.json()
+document.addEventListener('DOMContentLoaded', getBookList);
 
-
-
-console.log(books)
+function getBookList() {
+    // Change the URL to the actual endpoint of your server
+    fetch('http://localhost:3001/listBooks')
+        .then(response => response.json())
+        .then(data => displayBookList(data))
+        .catch(error => console.error('Error:', error));
 }
 
+function displayBookList(books) {
+    const bookListElement = document.getElementById('bookList');
+    const form = document.getElementById('bookForm');
 
-admin()
+    // Clear existing content
+    bookListElement.innerHTML = '';
+
+    // Display each book in the list with an input field for quantity
+    books.forEach(book => {
+        const listItem = document.createElement('li');
+        const inputField = document.createElement('input');
+        const updateButton = document.createElement('button');
+
+        listItem.textContent = `${book.title} - Quantity: `;
+        inputField.type = 'number';
+        inputField.value = book.quantity;
+        inputField.name = `book_${book.id}`;
+
+        updateButton.type = 'button';
+        updateButton.textContent = 'Save';
+        updateButton.addEventListener('click', () => updateQuantity(book.id, inputField.value));
+
+        listItem.appendChild(inputField);
+        listItem.appendChild(updateButton);
+        bookListElement.appendChild(listItem);
+    });
+
+        // Prevent the form from submitting and handle the update in JavaScript
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            updateQuantities();
+        });
+}
+
+function updateQuantity(bookID, newQuantity) {
+    // Implement the logic to update a single book's quantity
+    console.log(`Updating quantity for Book ${book.id} to ${newQuantity}`);
+}
+function updateQuantities() {
+    // Implement the logic to update quantities for all books
+    console.log('Updating quantities for all books');
+}
